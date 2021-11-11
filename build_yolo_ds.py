a553eef8bf093d04482801a64e634a42208e6767
Reza Baharani <mbaharan@uncc.edu>
2021-11-10 20:31:52 -0500

    Adding the configuration for button-up appraoch.

--- build_yolo_ds.py
+++ build_yolo_ds.py
@@ -3,8 +3,13 @@ from .deep_sort import build_tracker
 
 
 def build_yolo_ds(args, use_cuda=True):
-    detector = build_detector(
-        args.yolo_model_path, args.yolo_anchors_path, args.yolo_classes_path)
+    
+    detector = None
+
+    if not args.bottom_up_approach:
+        detector = build_detector(
+            args.yolo_model_path, args.yolo_anchors_path, args.yolo_classes_path)
+
     deepsort = build_tracker(args, use_cuda)
 
     return detector, deepsort
