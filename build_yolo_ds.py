703fc0c3e5ab5a6d48869bf1fc9e8fe81a4b6294
Reza Baharani <mbaharan@uncc.edu>
2021-11-16 16:10:33 -0500

    Can load the model. Working on SmartCity Class.

--- build_yolo_ds.py
+++ build_yolo_ds.py
@@ -8,7 +8,7 @@ def build_yolo_ds(args, use_cuda=True):
 
     if not args.bottom_up_approach:
         detector = build_detector(
-            args.yolo_model_path, args.yolo_anchors_path, args.yolo_classes_path)
+            args.yolo_model_path, args.yolo_anchors_path, args.classes_path)
 
     deepsort = build_tracker(args, use_cuda)
 
--- deep_sort/__init__.py
+++ deep_sort/__init__.py
@@ -5,7 +5,7 @@ __all__ = ['DeepSort', 'build_tracker']
 
 
 def build_tracker(args, use_cuda):
-    return DeepSort(args.ds_model_path, namesfile=args.yolo_classes_path,  # namesfile=cfg.DEEPSORT.CLASS_NAMES,
+    return DeepSort(args.ds_model_path, namesfile=args.classes_path,  # namesfile=cfg.DEEPSORT.CLASS_NAMES,
                     max_dist=0.2, min_confidence=0.1,
                     nms_max_overlap=0.5, max_iou_distance=0.7,
                     max_age=70, n_init=3, nn_budget=100, use_cuda=True)
