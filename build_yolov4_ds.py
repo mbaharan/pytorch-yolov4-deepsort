d767481ded875ab2774a03f86d23e5468f94d7c2
Reza Baharani <mbaharan@uncc.edu>
2022-05-16 18:33:49 -0400

    Changed functoin to support Yv5.

--- build_yolov4_ds.py
+++ build_yolov4_ds.py
@@ -2,7 +2,7 @@ from .detector import build_detector
 from .deep_sort import build_tracker
 
 
-def build_yolo_ds(args, client_cfg, logger, use_cuda=True):
+def build_yolov4_ds(args, client_cfg, logger, use_cuda=True):
     detector = build_detector(
         args.yolo_model_path, args.yolo_anchors_path, client_cfg.DeepSORT.YOLO_Classes_Path, logger)
     deepsort = build_tracker(client_cfg, logger, use_cuda)
